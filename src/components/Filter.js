632eaad444bd399fad9464c7ab1be6b2be927e5d
hanaMohammedAbdullah <hana.muhammed.abdulla@gmail.com>
2024-04-10 23:21:04 +0300

    Complete Assigment

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,14 +1,24 @@
 import React from "react";
 
-function Filter({ onCategoryChange }) {
+function Filter({ onCategoryChange, onSearchChange, search }) {
+  const searchandler = (event) => {
+    onSearchChange(event);
+  };
+
   return (
-    <div className="Filter">
-      <input type="text" name="search" placeholder="Search..." />
-      <select name="filter" onChange={onCategoryChange}>
-        <option value="All">Filter by category</option>
-        <option value="Produce">Produce</option>
-        <option value="Dairy">Dairy</option>
-        <option value="Dessert">Dessert</option>
+    <div className='Filter'>
+      <input
+        type='text'
+        name='search'
+        placeholder='Search...'
+        value={search}
+        onChange={searchandler}
+      />
+      <select name='filter' onChange={onCategoryChange}>
+        <option value='All'>Filter by category</option>
+        <option value='Produce'>Produce</option>
+        <option value='Dairy'>Dairy</option>
+        <option value='Dessert'>Dessert</option>
       </select>
     </div>
   );
--- src/components/ItemForm.js
+++ src/components/ItemForm.js
@@ -1,24 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import { v4 as uuid } from "uuid";
 
 function ItemForm(props) {
+  const [itemName, setItemName] = useState("");
+  const [itemCategory, setItemCategory] = useState("Produce");
+
+  const newItem = {
+    id: uuid(), // the `uuid` library can be used to generate a unique id
+    name: itemName,
+    category: itemCategory,
+  };
+  const handleNameChange = (event) => {
+    setItemName(event.target.value);
+  };
+  const handleCategoryChange = (event) => {
+    setItemCategory(event.target.value);
+  };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(newItem);
+    props.onItemFormSubmit(newItem);
+  };
+
   return (
-    <form className="NewItem">
+    <form className='NewItem' onSubmit={handleSubmit}>
       <label>
         Name:
-        <input type="text" name="name" />
+        <input type='text' name='Name' onChange={handleNameChange} />
       </label>
 
       <label>
         Category:
-        <select name="category">
-          <option value="Produce">Produce</option>
-          <option value="Dairy">Dairy</option>
-          <option value="Dessert">Dessert</option>
+        <select name='category' onChange={handleCategoryChange}>
+          <option value='Produce'>Produce</option>
+          <option value='Dairy'>Dairy</option>
+          <option value='Dessert'>Dessert</option>
         </select>
       </label>
 
-      <button type="submit">Add to List</button>
+      <button type='submit'>Add to List</button>
     </form>
   );
 }
